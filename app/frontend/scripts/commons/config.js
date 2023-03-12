7bf4f75f7b1abaaf3f0069fb64dbba4af094b466
andrian <andrian@klikcair.com>
2023-03-13 00:03:48 +0700

    feat: add product repository

---
+++ app/frontend/scripts/commons/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_BASEURL: 'http://localhost:3000',
+};
+
+export default CONFIG;
---
+++ app/frontend/scripts/repositories/ProductRepo.js
@@ -0,0 +1,28 @@
+import CONFIG from '../commons/config';
+
+const ProductRepo = {
+  getAllProductTags: async () => {
+    const response = await fetch(`${CONFIG.API_BASEURL}/product-tags`);
+    const responseJSON = await response.json();
+    return responseJSON;
+  },
+
+  getAllProducts: async (tags) => {
+    let url = `${CONFIG.API_BASEURL}/products`;
+    if (tags.length > 0) {
+      url += `?tags_like=${tags.join(',')}`;
+    }
+
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    return responseJSON;
+  },
+
+  getSpecificProductById: async (productId) => {
+    const response = await fetch(`${CONFIG.API_BASEURL}/products/${productId}`);
+    const responseJSON = await response.json();
+    return responseJSON;
+  },
+};
+
+export default ProductRepo;
